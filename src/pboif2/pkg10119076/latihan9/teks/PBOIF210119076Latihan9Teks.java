a1096dc620341c398edde0876ddbca2c88a42db0
ACER <ACER@DESKTOP-JMQ2UKH>
2020-10-17 17:31:16 +0700

    PBOIF2-10119076-Latihan9-Teks

---
+++ src/pboif2/pkg10119076/latihan9/teks/PBOIF210119076Latihan9Teks.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119076.latihan9.teks;
+
+/**
+ *
+ * @author ACER
+ * NAMA     : Sulthon Naufal Akmal
+ * KELAS    : IF2
+ * NIM      : 10119076
+ * Deskripsi Program : Teks
+ */
+public class PBOIF210119076Latihan9Teks {
+
+   
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char: " + tab + "berisi " + c);
+    }
+    
+}
